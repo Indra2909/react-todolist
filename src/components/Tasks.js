c878ba3ef1b89d9742cdb5d1de765f984c729ce3
Indra <110344970+Indra2909@users.noreply.github.com>
2022-09-02 16:08:56 +0700

    Add Netlify Dependencies

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { FaToiletPaperSlash } from "react-icons/fa";
 import Task from "./Task";
 
 const important = "important";
